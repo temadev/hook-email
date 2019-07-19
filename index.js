dfedabfa0ec4db7406fd5f5c0807c274486a003f
Artem Kashin <dev.tema@gmail.com>
2019-07-19 09:37:49 +0300

    update message

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const ini = require('ini');
 const config = ini.parse(fs.readFileSync('./.git/config', 'utf-8'));
 const args = process.argv.slice(2);
 
+if (!args[0]) {
+  console.log(`
+  ==========
+  Set your email pattern as the last script parameter.
+  ==========
+  `
+  )
+  return process.exit(1);
+}
+
 if (
   !(
-    args.length &&
     config &&
     config.user &&
     config.user.email &&
@@ -16,7 +25,9 @@ if (
   console.log(`
   ==========
   Only ${args[0]} email allowed for commiting
-  update your .git/config
+
+  you can set the email for this repository by using command:
+  git config user.email "you@domain.com"
   ==========
   `
   )
